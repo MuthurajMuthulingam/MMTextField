45d9226584796b91c35aa50d2f1f343c5696be8e
Muthuraj Muthulingam <muthurajm@Muthus-MacBook-Pro.local>
2018-09-06 13:03:51 +0530

    Card Number TextField Validation

--- MMCardTextField.swift
+++ MMCardTextField.swift
@@ -1,6 +1,6 @@
 //
 //  MMCardTextField.swift
-//  NetworkTemplate
+//  MMTextField
 //
 //  Created by Muthuraj Muthulingam on 03/09/18.
 //  Copyright © 2018 Muthuraj Muthulingam. All rights reserved.
@@ -15,9 +15,21 @@ enum MMCardType {
     case jcb
     case discover
     case americanExpress
+    case none
+}
+
+extension MMCardType {
     
-    var cardNumberCount: Int {
+    var allowedNumberCount: Int {
         return self == .americanExpress ? 15 : 16
+    }
+    
+    var seperatorCharectorCount: Int {
+        return self == .americanExpress ? 2 : 3
+    }
+    
+    var totalCharectorsCount: Int {
+        return allowedNumberCount + seperatorCharectorCount
     }
 }
 
@@ -30,24 +42,56 @@ public class MMCardTextField: UITextField, UITextFieldDelegate {
     
     var cardFieldDelegate: MMCardTextFieldDelegate?
     
-    @IBInspectable var seperatorString: String = " "
-    var currentCardType: MMCardType? = nil
+    @IBInspectable var seperatorString: String = " " { // sepeartor charector
+        didSet {
+            designTextField()
+        }
+    }
+    @IBInspectable var placeHolderString: String = "*" { // a placeholder format string based on card type
+        didSet {
+            designTextField()
+        }
+    }
+    
+    var currentCardType: MMCardType = .none
 
     override public init(frame: CGRect) {
         super.init(frame: frame)
         prepareTextField()
+        designTextField()
     }
     
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         prepareTextField()
+        designTextField()
     }
     
     // MARK: - Private Helpers
     private func prepareTextField() {
         self.delegate = self
-        self.type
+        self.borderStyle = .line
         setupCardImageView()
+    }
+    
+    private func designTextField() {
+       placeholder = placeHolderString(forCardType: currentCardType)
+    }
+    
+    private func placeHolderString(forCardType cardType: MMCardType) -> String {
+        var finalPlaceHolderString = ""
+        for i in 1...cardType.totalCharectorsCount {
+            if cardType != .americanExpress && i % 5 == 0 { // non amex card spacing
+               finalPlaceHolderString += seperatorString
+            } else if cardType == .americanExpress { // amex card
+                if i == 5 || i == 12 { // hard coded for now, TODO: Find a equation here
+                    finalPlaceHolderString += seperatorString
+                }
+            } else {
+                finalPlaceHolderString += placeHolderString
+            }
+        }
+        return finalPlaceHolderString
     }
     
     private func setupCardImageView() {
@@ -57,14 +101,20 @@ public class MMCardTextField: UITextField, UITextFieldDelegate {
     }
     
     public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        guard let _ = Int(string) else {
+    
+        guard Int(string) != nil || string.count == 0 else { // handle space char
             return false
+        }
+        
+        let newString = (text ?? "") + string
+        if newString.count > currentCardType.totalCharectorsCount { // don't allow to exceed maximum charectors
+           return false
         }
         
         // handle paste operation
         // assumption, paste happens on multiple charectors
         if string.count > 1 {
-           let trimmedNumString = string.trim(numbers: true)
+           let trimmedNumString = string.numericValues()
             if trimmedNumString.count == 0 {
                 return false // as if no numbers on pasted string
             }
@@ -74,3 +124,19 @@ public class MMCardTextField: UITextField, UITextFieldDelegate {
         return true
     }
 }
+
+extension String {
+    
+    func numericValues() -> String {
+        return self.filter { strChar in
+            return Int("\(strChar)") == nil
+        }
+    }
+    
+    func alphabets() -> String {
+        return self.filter { strChar in
+            return Int("\(strChar)") != nil
+        }
+    }
+}
+
